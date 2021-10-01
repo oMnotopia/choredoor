57806bb4a6b88cef56cc52ac545bb7930e7903dc
Clayton Persinger <65939048+oMnotopia@users.noreply.github.com>
2021-10-01 13:46:35 -0700

    Working functionality, game can be broken by pressing enter on submit line when playing

--- userInputScript.js
+++ userInputScript.js
@@ -21,6 +21,7 @@ let currentlyPlaying = false;
 let currentWinCounter = 0;
 let bestWinCounter = 0;
 
+//Functions
 //Checks to see if the user has opened a door with the robot.
 const isBot = door => {
     return (door.src===botDoorPath) ? true:false;
@@ -31,7 +32,7 @@ const isClicked = door => {
     return (door.src===closedDoorPath) ? false:true;
 }
 
-//reduces numClosedDoors so when it hits 0 there is a winner.
+//Reduces numClosedDoors every time a door is clicked so when it hits 0 there is a winner.
 const playDoor = door => {
     numClosedDoors--;
     if(numClosedDoors===0) {
@@ -43,6 +44,7 @@ const playDoor = door => {
 
 //Assigns a random image to each door as the door is clicked.
 const randomChoreDoorGenerator = () => {
+    submitBtn.style.display = 'none'//Hide submit button (redisplays when new round is started)
     //If only one door left assign robot
     if ((numberOfRobots===numClosedDoors)&&(numberOfRobots!==0)) {
         return botDoorPath;
@@ -69,76 +71,12 @@ const storedUserInput = (userInputOfDoors) => {
     }
 }
 
-//Event listener 'click', starts/restarts the game.
-startButton.addEventListener('click', () => {
-    if(currentlyPlaying===false) startRound();
-})
-
-
-//Event listener 'click' on form button captures user input.
-submitBtn.addEventListener('click', ev => {
-    if(currentlyPlaying===true) return
-    currentlyPlaying=true;
-    ev.preventDefault();//stops the form submitting
-    
-    //Sets variables for parent node and door nodes
-    userInputOfDoors = document.getElementById('numOfDoors').value
-
-    //Stores user input in a function for next round
-    storedUserInput(userInputOfDoors)
-
-    //Needs to be converted from int to num in order for addition below.
-    numClosedDoors = parseInt(numClosedDoors,10)
-    userInputOfDoors = parseInt(userInputOfDoors, 10)
-    //Numeric values to decide how many loops are needed to complete addition/subtraction of door elements.
-    let addOrSubtractDoors = userInputOfDoors-numClosedDoors
-    let newDoorID = numClosedDoors+1
-    let removeDoorID = Math.abs(numClosedDoors)
-
-    numClosedDoors = userInputOfDoors;
-
-    console.log(addOrSubtractDoors)
-    //Determines if doors are going to be added/subtracted/or nothing done.
-    if (addOrSubtractDoors===0) return
-    //If doors added, create nodes and assign attributes.
-    if (addOrSubtractDoors>0) {
-        while(addOrSubtractDoors!==0) {
-
-            let newDoor = document.createElement("img")
-            let newDoorNumber = doorParentID.appendChild(newDoor)
-
-            newDoorNumber.setAttribute("id", "door"+newDoorID)
-            newDoorNumber.setAttribute("class", "door-frame")
-            newDoorNumber.setAttribute("src", closedDoorPath)
-            newDoorNumber.addEventListener('click', () => {
-                if((isClicked(newDoorNumber)===false)&&(currentlyPlaying===true)) {
-                    newDoorNumber.src = randomChoreDoorGenerator(addOrSubtractDoors);
-                    playDoor(newDoorNumber);     
-                }
-            })
-            //Variables to adjust for loop increase and door class names.
-            addOrSubtractDoors--
-            newDoorID++;
-        }
-    //If doors removed, remove nodes.    
-    } else {
-        console.log(addOrSubtractDoors)
-        while(addOrSubtractDoors!==0) {
-
-            let removeDoor = document.getElementById("door"+removeDoorID)
-            console.log(removeDoorID)
-
-            addOrSubtractDoors++
-            removeDoorID--
-        }  
-    }
-})
-
 //Restarts the game based off previous input value the user supplied.
 const startRound = () => {
     //Reset necessary values for new start to game.
+    currentlyPlaying=true;
+    submitBtn.style.display = ''//Redisplay submit button (hides when door is clicked)
     startButton.innerHTML = 'Good Luck!'
-    currentlyPlaying = true;
     numberOfRobots = 1;
     numClosedDoors = storedUserInput()//Gets stored input.
 
@@ -150,7 +88,7 @@ const startRound = () => {
         element.setAttribute("src", closedDoorPath) 
         element.addEventListener('click', () => {
             if((isClicked(element)===false)&&(currentlyPlaying===true)) {
-               element.src = randomChoreDoorGenerator(addOrSubtractDoors);
+                element.src = randomChoreDoorGenerator();
                 playDoor(element);     
             }
         })  
@@ -173,7 +111,77 @@ const gameOver = status => {
         currentScore.innerHTML = currentWinCounter;
         startButton.innerHTML = 'You Lose! Play again?'
     }
-    currentlyPlaying = false; //reset playing to false
+    currentlyPlaying = false; //Reset playing flag to false
 }
+
+function howManyDoors(ev) {
+    ev.preventDefault();//stops the form submitting
+    
+    //Sets variables for parent node and door nodes.
+    userInputOfDoors = document.getElementById('numOfDoors').value
+    let childOfDoors = doorParentID.childNodes;
+
+    //Needs to be converted from str to num in order for addition below.
+    numClosedDoors = parseInt(numClosedDoors,  10)
+    userInputOfDoors = parseInt(userInputOfDoors, 10)
+
+    //If user enters a value less than two, raise to two, or more than ten, lower to then.
+    if(userInputOfDoors<2) userInputOfDoors=2;
+    if(userInputOfDoors>10) userInputOfDoors=10;
+
+    //Stores user input in a function for next round
+    storedUserInput(userInputOfDoors)
+
+    //Numeric values to decide how many loops are needed to complete addition/subtraction of door elements.
+    let addOrSubtractDoors = userInputOfDoors-childOfDoors.length
+    let newDoorID = numClosedDoors+1
+    let removeDoorID = Math.abs(numClosedDoors)
+    numClosedDoors = userInputOfDoors;
+
+    currentlyPlaying=true;
+    //Determines if doors are going to be added/subtracted/or nothing done.
+    if (addOrSubtractDoors===0) return
+    //If doors added, then create nodes, assign attributes, and event listeners.
+    if (addOrSubtractDoors>0) {
+        while(addOrSubtractDoors!==0) {
+
+            let newDoor = document.createElement("img")
+            let newDoorNumber = doorParentID.appendChild(newDoor)
+
+            newDoorNumber.setAttribute("id", "door"+newDoorID)
+            newDoorNumber.setAttribute("class", "door-frame")
+            newDoorNumber.setAttribute("src", closedDoorPath)
+            newDoorNumber.addEventListener('click', () => {
+                if((isClicked(newDoorNumber)===false)&&(currentlyPlaying===true)) {
+                    newDoorNumber.src = randomChoreDoorGenerator(addOrSubtractDoors);
+                    playDoor(newDoorNumber);     
+                }
+            })
+            //Variables to adjust for loop increase and door class names.
+            addOrSubtractDoors--
+            newDoorID++;
+        }
+    //If doors removed, remove nodes containing doors.    
+    } else {
+        while(addOrSubtractDoors!==0) {
+
+            let removeDoor = document.getElementById("door"+removeDoorID)
+            doorParentID.removeChild(removeDoor)
+
+            addOrSubtractDoors++
+            removeDoorID--
+        }  
+    }
+}
+
+//Event Listeners
+//Event listener 'click', starts/restarts the game.
+startButton.addEventListener('click', () => {
+    if(currentlyPlaying===false) startRound();
+})
+
+
+//Event listener 'click' on form button that user submits.
+submitBtn.addEventListener('click', howManyDoors)
 
 
